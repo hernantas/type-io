bfb77f02b87c03b5446f59d293ae593ce78533ab
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-03-03 11:33:34 +0700

    Refactor basic spec test

--- packages/type-io-core/test/function/basic.spec.ts
+++ packages/type-io-core/test/function/basic.spec.ts
@@ -1,101 +1,87 @@
-import { expect } from 'chai'
+import { use, expect } from 'chai'
+import chaiDateTime from 'chai-datetime'
 import { PlainParser, Prop } from '../../src'
 
+use(chaiDateTime)
+
 describe('Basic functionality', () => {
-  class Basic {
+  class WithString {
     @Prop()
     typeString: string
 
+    constructor () {
+      this.typeString = ''
+    }
+  }
+  class WithNumber {
     @Prop()
     typeNumber: number
 
+    constructor () {
+      this.typeNumber = 0
+    }
+  }
+
+  class WithBoolean {
     @Prop()
     typeBoolean: boolean
 
     constructor () {
-      this.typeString = ''
-      this.typeNumber = 0
       this.typeBoolean = false
+    }
+  }
+  class WithDate {
+    @Prop()
+    typeDate: Date
+
+    constructor () {
+      this.typeDate = new Date()
     }
   }
 
   const parser = new PlainParser()
 
-  it('Decode from plain object', () => {
+  it('Decode/Encode from/to plain (String)', () => {
     const plain = {
-      typeString: 'This is String',
-      typeNumber: 1,
-      typeBoolean: true
-    }
-    const decode = parser.decode(plain, Basic)
-    expect(decode).to.have.property('typeString', plain.typeString)
-    expect(decode).to.have.property('typeNumber', plain.typeNumber)
-    expect(decode).to.have.property('typeBoolean', plain.typeBoolean)
-  })
-
-  it('Encode from object', () => {
-    const value = new Basic()
-    value.typeString = 'This is String'
-    value.typeNumber = 1
-    value.typeBoolean = true
-
-    const encode = parser.encode(value)
-    expect(encode).to.have.property('typeString', value.typeString)
-    expect(encode).to.have.property('typeNumber', value.typeNumber)
-    expect(encode).to.have.property('typeBoolean', value.typeBoolean)
-  })
-
-  it('Decode from plain to object and encode to plain object', () => {
-    const plain = {
-      typeString: 'This is String',
-      typeNumber: 1,
-      typeBoolean: true
+      typeString: 'This is String'
     }
 
-    const decode = parser.decode(plain, Basic)
+    const decode = parser.decode(plain, WithString)
     const encode = parser.encode(decode)
     expect(decode).to.have.property('typeString', plain.typeString)
-    expect(decode).to.have.property('typeNumber', plain.typeNumber)
-    expect(decode).to.have.property('typeBoolean', plain.typeBoolean)
     expect(encode).to.have.property('typeString', plain.typeString)
+  })
+
+  it('Decode/Encode from/to plain (Number)', () => {
+    const plain = {
+      typeNumber: 13
+    }
+
+    const decode = parser.decode(plain, WithNumber)
+    const encode = parser.encode(decode)
+    expect(decode).to.have.property('typeNumber', plain.typeNumber)
     expect(encode).to.have.property('typeNumber', plain.typeNumber)
+  })
+
+  it('Decode/Encode from/to plain (Boolean)', () => {
+    const plain = {
+      typeBoolean: true
+    }
+
+    const decode = parser.decode(plain, WithBoolean)
+    const encode = parser.encode(decode)
+    expect(decode).to.have.property('typeBoolean', plain.typeBoolean)
     expect(encode).to.have.property('typeBoolean', plain.typeBoolean)
   })
 
-  class WithDate {
-    @Prop()
-    created: Date
-
-    constructor () {
-      this.created = new Date()
-    }
-  }
-
-  it('Decode from plain object with Date', () => {
+  it('Decode/Encode from/to plain (Date)', () => {
     const plain = {
-      created: new Date()
+      typeDate: new Date()
     }
 
     const decode = parser.decode(plain, WithDate)
-    expect(decode).to.have.property('created', plain.created)
-  })
-
-  it('Decode from plain object with Date from string/number', () => {
-    const plainStr = {
-      created: 'October 13, 2014 11:13:00'
-    }
-    const plainNum = {
-      created: 1614235210802
-    }
-
-    const dateStr = new Date(plainStr.created)
-    const dateNum = new Date(plainNum.created)
-
-    const decodeStr = parser.decode(plainStr, WithDate)
-    const decodeNum = parser.decode(plainNum, WithDate)
-    expect(decodeStr).to.have.property('created')
-    expect(decodeStr.created.getTime()).to.be.eq(dateStr.getTime())
-    expect(decodeNum).to.have.property('created')
-    expect(decodeNum.created.getTime()).to.be.eq(dateNum.getTime())
+    const encode = parser.encode(decode)
+    expect(decode).to.have.property('typeDate').and.equalTime(plain.typeDate)
+    expect(encode).to.to.have.property('typeDate').and.equalTime(plain.typeDate)
   })
 })
