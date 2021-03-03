27c68170c02d1e10cec4b14abf40aa9700bc5326
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-03-03 13:21:11 +0700

    Simplify nested and optional test

--- packages/type-io-core/test/function/nested.spec.ts
+++ packages/type-io-core/test/function/nested.spec.ts
@@ -30,36 +30,7 @@ describe('Parse Nested functionality', () => {
 
   const parser = new PlainParser()
 
-  it('Decode from plain object', () => {
-    const plain = {
-      name: 'This is parent',
-      child: {
-        name: 'This is child',
-        age: 13
-      }
-    }
-
-    const decode = parser.decode(plain, Parent)
-    expect(decode).to.have.property('name', plain.name)
-    expect(decode).to.have.property('child')
-    expect(decode.child).to.have.property('name', plain.child.name)
-    expect(decode.child).to.have.property('age', plain.child.age)
-  })
-
-  it('Encode from object', () => {
-    const value = new Parent()
-    value.name = 'This is parent'
-    value.child.name = 'This is child'
-    value.child.age = 12
-
-    const encode = parser.encode(value)
-    expect(encode).to.have.property('name', value.name)
-    expect(encode).to.have.property('child')
-    expect(encode).to.have.nested.property('child.name', value.child.name)
-    expect(encode).to.have.nested.property('child.age', value.child.age)
-  })
-
-  it('Decode from plain object and encode to plain object', () => {
+  it('Decode/Encode from plain', () => {
     const plain = {
       name: 'This is parent',
       child: {
--- packages/type-io-core/test/function/optional.spec.ts
+++ packages/type-io-core/test/function/optional.spec.ts
@@ -16,63 +16,7 @@ describe('Optional property functionality', () => {
 
   const parser = new PlainParser()
 
-  it('Decode from plain object (all property)', () => {
-    const plain = {
-      reqString: 'This is required string',
-      optString: 'This is optional string'
-    }
-
-    const decode = parser.decode(plain, Optional)
-    expect(decode).to.have.property('reqString', plain.reqString)
-    expect(decode).to.have.property('optString', plain.optString)
-  })
-
-  it('Decode from plain object (required property only)', () => {
-    const plain = {
-      reqString: 'This is required string'
-    }
-
-    const decode = parser.decode(plain, Optional)
-    expect(decode).to.have.property('reqString', plain.reqString)
-    expect(decode).to.not.have.property('optString')
-  })
-
-  it('Decode from plain object (missing required property)', () => {
-    const plain = {
-      optString: 'This is optional string'
-    }
-
-    expect(() => parser.decode(plain, Optional)).to.throw()
-  })
-
-  it('Encode from object (all property)', () => {
-    const value = new Optional()
-    value.reqString = 'This is required string'
-    value.optString = 'This is optional string'
-
-    const encode = parser.encode(value)
-    expect(encode).to.have.property('reqString', value.reqString)
-    expect(encode).to.have.property('optString', value.optString)
-  })
-
-  it('Encode from object (required property only)', () => {
-    const value = new Optional()
-    value.reqString = 'This is required string'
-
-    const encode = parser.encode(value)
-    expect(encode).to.have.property('reqString', value.reqString)
-    expect(encode).to.not.have.property('optString')
-  })
-
-  it('Encode from object (missing required property)', () => {
-    const value = new Optional()
-    delete (value as any).reqString
-    value.optString = 'This is optional string'
-
-    expect(() => parser.encode(value)).to.throw()
-  })
-
-  it('Decode from plain object and encode to plain object (all property)', () => {
+  it('Decode/Encode from plain (all property)', () => {
     const plain = {
       reqString: 'This is required string',
       optString: 'This is optional string'
@@ -80,24 +24,28 @@ describe('Optional property functionality', () => {
 
     const decode = parser.decode(plain, Optional)
     const encode = parser.encode(decode)
-
     expect(decode).to.have.property('reqString', plain.reqString)
     expect(decode).to.have.property('optString', plain.optString)
     expect(encode).to.have.property('reqString', plain.reqString)
     expect(encode).to.have.property('optString', plain.optString)
   })
 
-  it('Decode from plain object and encode to plain object (required property only)', () => {
+  it('Decode/Encode from plain (required property)', () => {
     const plain = {
       reqString: 'This is required string'
     }
 
     const decode = parser.decode(plain, Optional)
     const encode = parser.encode(decode)
-
     expect(decode).to.have.property('reqString', plain.reqString)
-    expect(decode).to.not.have.property('optString')
     expect(encode).to.have.property('reqString', plain.reqString)
-    expect(encode).to.not.have.property('optString')
+  })
+
+  it('Decode/Encode from plain (missing property)', () => {
+    const plain = {
+      optString: 'This is optional string'
+    }
+
+    expect(() => parser.decode(plain, Optional)).to.throws()
   })
 })
