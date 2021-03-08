c632ca4f4ee4a0237d2112c39cb6a44111375959
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-03-08 11:13:29 +0700

    Split `codec` into `Decoder` and `Encoder`

--- packages/core/src/codec/codec.ts
+++ packages/core/src/codec/codec.ts
@@ -1,8 +1,5 @@
-import { AnyParamConstructor } from '../type'
+import { Decoder } from './decoder'
+import { Encoder } from './encoder'
 
-export interface Codec<T, O = T, I = unknown> {
-  readonly type: AnyParamConstructor
-
-  decode(value: I): T
-  encode(value: T): O
+export interface Codec<T, O = T, I = unknown> extends Decoder<T, I>, Encoder<T, O> {
 }
