b34facbdeb6919e23731866c54ce72c5bddac305
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-03-10 13:57:56 +0700

    Refactor `Parser`

--- packages/core/src/codec/parser.ts
+++ packages/core/src/codec/parser.ts
@@ -3,15 +3,15 @@ import { AnyObject, AnyParamConstructor } from '../type'
 import { Codec } from './codec'
 
 export class Parser {
-  codecs: Codec<unknown>[]
+  private codecs: Codec<unknown>[]
 
   constructor (codecs: AnyParamConstructor<Codec<unknown>>[]) {
     this.codecs = codecs.map(CodecConstructor => new CodecConstructor())
   }
 
-  decode <T, I> (input: I[], Type: AnyParamConstructor<T>, traces?: string[]): T[]
-  decode <T, I> (input: I, Type: AnyParamConstructor<T>, traces?: string[]): T
-  decode <T, I> (input: I | I[], Type: AnyParamConstructor<T>, traces?: string[]): T | T[] {
+  decode <T, I> (input: I[], Type: AnyParamConstructor<T>): T[]
+  decode <T, I> (input: I, Type: AnyParamConstructor<T>): T
+  decode <T, I> (input: I | I[], Type: AnyParamConstructor<T>): T | T[] {
     if (Array.isArray(input)) {
       const output: T[] = []
       for (const element of input) {
@@ -21,52 +21,12 @@ export class Parser {
     }
 
     const codec = this.findCodec(Type)
-    if (codec !== undefined) {
-      try {
-        return codec.decode(input)
-      } catch (e) {
-        throw new Error(traces !== undefined
-          ? `Error when decoding '${traces.join('.')}' property. Reason: ${e}`
-          : `Error when decoding value. Reason: ${e}`
-        )
-      }
-    } else if (typeof input === 'object' && input !== null) {
-      // codec is not found but its an object
-      const output = new Type()
-      const propDefs = Metadata.getTypeDef(Type)
-
-      for (const propDef of propDefs) {
-        const inPropName = propDef.inName as keyof I
-        const outPropName = propDef.name as keyof T
-
-        // create traces
-        const propTraces = traces !== undefined
-          ? traces.concat(propDef.name)
-          : [propDef.name]
-
-        if (inPropName in input) {
-          output[outPropName] = this.decode(
-            input[inPropName],
-            propDef.type,
-            propTraces
-          ) as T[keyof T]
-        } else if (!propDef.optional) {
-          throw new Error(`'${propTraces.join('.')}' property is required but do not exist in given input when decoding`)
-        }
-      }
-
-      return output
-    } else {
-      throw new Error(traces !== undefined
-        ? `No codec found for property '${traces.join('.')}' when decoding`
-        : 'No codec found for given input when decoding'
-      )
-    }
+    return codec.decode(input)
   }
 
-  encode <T> (input: T[], Type?: AnyParamConstructor<T>, traces?: string[]): unknown[]
-  encode <T> (input: T, Type?: AnyParamConstructor<T>, traces?: string[]): unknown
-  encode <T> (input: T | T[], Type?: AnyParamConstructor<T>, traces?: string[]): unknown | unknown[] {
+  encode <T> (input: T[], Type?: AnyParamConstructor<T>): unknown[]
+  encode <T> (input: T, Type?: AnyParamConstructor<T>): unknown
+  encode <T> (input: T | T[], Type?: AnyParamConstructor<T>): unknown | unknown[] {
     if (Type === undefined) {
       Type = findInputConstructor(input)
     }
@@ -80,53 +40,52 @@ export class Parser {
     }
 
     const codec = this.findCodec(Type)
-    if (codec !== undefined) {
-      try {
-        return codec.encode(input)
-      } catch (e) {
-        throw new Error(traces !== undefined
-          ? `Error when encoding '${traces.join('.')}' property. Reason: ${e}`
-          : `Error when encoding value. Reason: ${e}`
-        )
-      }
-    } else if (typeof input === 'object' && input !== null) {
-      // codec is not found but its an object
-      const output: AnyObject = {}
-      const propDefs = Metadata.getTypeDef(Type)
-
-      for (const propDef of propDefs) {
-        const inPropName = propDef.inName as keyof T
-        const outPropName = propDef.name
-
-        // create traces
-        const propTraces = traces !== undefined
-          ? traces.concat(propDef.name)
-          : [propDef.name]
-
-        if (inPropName in input) {
-          output[outPropName] = this.encode(
-            input[inPropName],
-            propDef.type,
-            propTraces
-          ) as T[keyof T]
-        } else if (!propDef.optional) {
-          throw new Error(`'${propTraces.join('.')}' property is required but do not exist in given input when encoding`)
-        }
-      }
-
-      return output
-    } else {
-      throw new Error(traces !== undefined
-        ? `No codec found for property '${traces.join('.')}' when encoding`
-        : 'No codec found for given input when encoding'
-      )
-    }
+    return codec.encode(input)
   }
 
-  private findCodec <T> (type: AnyParamConstructor<T>): Codec<T, unknown> | undefined {
+  private findCodec <T> (Type: AnyParamConstructor<T>): Codec<T, unknown> {
     for (const codec of this.codecs) {
-      if (codec.type === type) {
+      if (codec.type === Type) {
         return codec as Codec<T, unknown>
+      }
+    }
+
+    const codec = this.createCodec(Type)
+    this.codecs.push(codec)
+    return codec
+  }
+
+  private createCodec <T, I extends AnyObject> (Type: AnyParamConstructor<T>): Codec<T, AnyObject, I> {
+    const propDefs = Metadata.getTypeDef(Type)
+    return {
+      type: Type,
+      decode: (input: I): T => {
+        const output = new Type()
+        for (const propDef of propDefs) {
+          const inPropName = propDef.inName as keyof I
+          const outPropName = propDef.name as keyof T
+
+          if (inPropName in input) {
+            output[outPropName] = this.decode(input[inPropName], propDef.type) as T[keyof T]
+          } else if (!propDef.optional) {
+            throw new Error(`'${propDef.inName}' property is required but do not exist in given input when decoding`)
+          }
+        }
+        return output
+      },
+      encode: (input: T): AnyObject => {
+        const output: AnyObject = {}
+        for (const propDef of propDefs) {
+          const inPropName = propDef.name as keyof T
+          const outPropName = propDef.outName
+
+          if (inPropName in input) {
+            output[outPropName] = this.encode(input[inPropName], propDef.type) as T[keyof T]
+          } else if (!propDef.optional) {
+            throw new Error(`'${propDef.name}' property is required but do not exist in given input when encoding`)
+          }
+        }
+        return output
       }
     }
   }
