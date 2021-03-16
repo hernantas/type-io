d190290994e937564ecb65a7f4e9f779dba1ea83
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-03-16 15:59:29 +0700

    Change array checking method in `findInputConstructor`

--- packages/core/src/codec/utils.ts
+++ packages/core/src/codec/utils.ts
@@ -1,7 +1,7 @@
 import { AnyParamConstructor } from '../type'
 
 export function findInputConstructor <T> (input: T | T[]): AnyParamConstructor<T> {
-  if (input instanceof Array) {
+  if (Array.isArray(input)) {
     if (input.length > 0) {
       return Object.getPrototypeOf(input[0]).constructor as AnyParamConstructor<T>
     } else {
