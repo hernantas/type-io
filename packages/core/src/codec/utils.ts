f8e444cb63a40ca6fbbef522a679702c4198a388
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-03-17 11:40:04 +0700

    Add utils function `findCtor`

--- packages/core/src/codec/utils.ts
+++ packages/core/src/codec/utils.ts
@@ -4,13 +4,26 @@ import { TargetTypes } from './target-types'
 export function findTargetType <T> (input: T[]): TargetType<T[]>
 export function findTargetType <T> (input: T): TargetType<T>
 export function findTargetType <T> (input: T | T[]): TargetType<T> | TargetType<T[]> {
+  try {
+    const ctor = findCtor(input)
+
+    if (Array.isArray(input)) {
+      return TargetTypes.array(ctor)
+    }
+
+    return ctor
+  } catch (e) {
+    throw new Error('Cannot find target type from empty array')
+  }
+}
+
+export function findCtor <T> (input: T | T[]): AnyParamConstructor<T> {
   if (Array.isArray(input)) {
     if (input.length > 0) {
-      return TargetTypes.array(Object.getPrototypeOf(input[0]).constructor as AnyParamConstructor<T>)
+      return Object.getPrototypeOf(input[0]).constructor as AnyParamConstructor<T>
     } else {
-      throw new Error('Cannot find target type from empty array')
+      throw new Error('Cannot find target constructor from empty array')
     }
   }
-
   return Object.getPrototypeOf(input).constructor as AnyParamConstructor<T>
 }
