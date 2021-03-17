6b26827b3e56b5e04eac75b64fb28a9ddb85e53d
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-03-17 11:40:42 +0700

    Add doc to `TargetTypes.array`

--- packages/core/src/codec/target-types.ts
+++ packages/core/src/codec/target-types.ts
@@ -1,6 +1,12 @@
 import { AnyParamConstructor, TargetType } from '../type'
 
 export class TargetTypes {
+  /**
+   * Construct `TargetType` from given type that describe it as `Array` of given type
+   *
+   * @param type Type of element in array
+   * @returns Array target type for given type
+   */
   static array <T> (type: AnyParamConstructor<T>): TargetType<T[]> {
     return {
       type: Array,
