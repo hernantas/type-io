135a4b63a502d4430a4edcc4fe8475ccef42191a
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-03-22 12:11:10 +0700

    Add codec for `ObjectId

---
+++ packages/bson/src/plain/objectid-bson-codec.ts
@@ -0,0 +1,22 @@
+import { Codec } from '@type-io/core'
+import { ObjectID, ObjectId } from 'bson'
+
+export class ObjectIdBsonCodec implements Codec<string, ObjectId> {
+  type = [String, ObjectId]
+
+  decode (value: unknown): string {
+    if (value instanceof ObjectId || value instanceof ObjectID) {
+      return value.toHexString()
+    }
+
+    if (typeof value === 'string') {
+      return ObjectId.createFromHexString(value).toHexString()
+    }
+
+    throw new Error('Unknown value type')
+  }
+
+  encode (value: string): ObjectId {
+    return ObjectId.createFromHexString(value)
+  }
+}
