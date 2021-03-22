f6ef07c571ddbc10038e8420d8ac9198f06891f7
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-03-22 12:21:15 +0700

    Add `PlainBsonParser` parser

---
+++ packages/bson/src/plain/plain-bson-parser.ts
@@ -0,0 +1,30 @@
+import { AnyParamConstructor, Codec, Parser } from '@type-io/core'
+import { BooleanBsonCodec } from './boolean-bson-codec'
+import { BufferBsonCodec } from './buffer-bson-codec'
+import { DateBsonCodec } from './date-bson-codec'
+import { Decimal128BsonCodec } from './decimal128-bson-codec'
+import { DoubleBsonCodec } from './double-bson-codec'
+import { Int32BsonCodec } from './int32-bson-codec'
+import { Int64BsonCodec } from './int64-bson-codec'
+import { NumberBsonCodec } from './number-bson-codec'
+import { ObjectIdBsonCodec } from './objectid-bson-codec'
+import { StringBsonCodec } from './string-bson-codec'
+import { TimestampBsonCodec } from './timestamp-bson-codec'
+
+export class PlainBsonParser extends Parser {
+  constructor (codecs?: AnyParamConstructor<Codec<unknown>>[]) {
+    super((codecs !== undefined ? codecs : []).concat([
+      StringBsonCodec,
+      NumberBsonCodec,
+      BooleanBsonCodec,
+      DateBsonCodec,
+      Int32BsonCodec,
+      Int64BsonCodec,
+      DoubleBsonCodec,
+      Decimal128BsonCodec,
+      ObjectIdBsonCodec,
+      BufferBsonCodec,
+      TimestampBsonCodec
+    ]))
+  }
+}
