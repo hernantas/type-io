ae60c32c3da44a3dcbd36fd44551460c0c4db275
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-04-01 11:50:05 +0700

    Add docs to `TargetType`

--- packages/core/src/type/target-type.ts
+++ packages/core/src/type/target-type.ts
@@ -1,5 +1,8 @@
 import { AnyParamConstructor } from './any-param-constructor'
 
+/**
+ * Allowed target type used for decoding/encoding
+ */
 export type TargetType<T = unknown> =
   | AnyParamConstructor<T>
   | AnyParamConstructor[]
