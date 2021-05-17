48564f74680da9901864f9e2a88002a20a214247
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-05-17 16:55:17 +0700

    Refine docs on `Decoder` & `Encoder`

--- packages/type-io-core/src/core/decoder.ts
+++ packages/type-io-core/src/core/decoder.ts
@@ -11,6 +11,7 @@ export interface Decoder<T, I = unknown> {
    * Decode given value into `<T>` type
    *
    * @param value value to be decoded
+   * @param options options to be used to decode
    */
   decode(value: I, options?: CodecOption): T
 }
--- packages/type-io-core/src/core/encoder.ts
+++ packages/type-io-core/src/core/encoder.ts
@@ -11,6 +11,7 @@ export interface Encoder<T, O = T> {
    * Encode given value into `<O>` type
    *
    * @param value value to be encoded
+   * @param options options to be used to encode
    */
   encode(value: T, options?: CodecOption): O
 }
