9817333b0c4b90dcd197b93b98a27d77554db277
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-05-26 14:00:12 +0700

    Add `LiteralIdentity`

---
+++ packages/type-io-core/src/type/LiteralIdentity.ts
@@ -0,0 +1,11 @@
+import { LiteralValue } from './LiteralValue'
+import { TypeIdentity } from './TypeIdentity'
+import { TypeKind } from './TypeKind'
+
+/**
+ * `TypeIdentity` that provides information about literal value of `<T>` type
+ */
+export interface LiteralIdentity<T extends LiteralValue> extends TypeIdentity<T> {
+  readonly kind: TypeKind.Literal
+  readonly value: T
+}
