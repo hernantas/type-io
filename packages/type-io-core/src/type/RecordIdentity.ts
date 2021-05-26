550201e62bdade099b0d44a4fcf20d427673ddb3
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-05-26 14:10:32 +0700

    Add `RecordIdentity`

--- packages/type-io-core/src/type/RecordIdentity.ts
+++ packages/type-io-core/src/type/RecordIdentity.ts
@@ -1,12 +1,12 @@
 import { RecordValue } from './RecordValue'
 import { TypeIdentity } from './TypeIdentity'
 import { TypeKind } from './TypeKind'
-import { TargetPropOf } from './util/TargetPropOf'
+import { TargetRecordOf } from './util/TargetRecordOf'
 
 /**
  * `TypeIdentity` that provides information about property of plain object on `<T>` type
  */
 export interface RecordIdentity<T extends RecordValue> extends TypeIdentity<T> {
   readonly kind: TypeKind.Record
-  readonly props: TargetPropOf<T>
+  readonly props: TargetRecordOf<T>
 }
