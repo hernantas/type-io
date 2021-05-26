75cda1ab54341d950150e78cec92613b9927353a
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-05-26 14:10:22 +0700

    Add `ArrayIdentity`

---
+++ packages/type-io-core/src/type/ArrayIdentity.ts
@@ -0,0 +1,9 @@
+import { TargetType } from './TargetType';
+import { TypeIdentity } from './TypeIdentity';
+import { TypeKind } from './TypeKind';
+
+
+export interface ArrayIdentity<T> extends TypeIdentity<T[]> {
+  readonly kind: TypeKind.Array;
+  readonly type: TargetType<T>;
+}
