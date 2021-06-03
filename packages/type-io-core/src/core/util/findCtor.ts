e3e3be7fea8dcc05aebd31fd35d047a845f1c8ca
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-06-03 15:07:49 +0700

    Simplify `findCtor`

--- packages/type-io-core/src/core/util/findCtor.ts
+++ packages/type-io-core/src/core/util/findCtor.ts
@@ -6,13 +6,6 @@ import { ConstructorValue } from '../../type'
  * @param input An instance of object
  * @returns Constructor of given value
  */
-export function findCtor <T> (input: T | T[]): ConstructorValue<T> {
-  if (Array.isArray(input)) {
-    if (input.length > 0) {
-      return Object.getPrototypeOf(input[0]).constructor as ConstructorValue<T>
-    } else {
-      throw new Error('Cannot find target constructor from empty array')
-    }
-  }
+export function findCtor <T> (input: T): ConstructorValue<T> {
   return Object.getPrototypeOf(input).constructor as ConstructorValue<T>
 }
