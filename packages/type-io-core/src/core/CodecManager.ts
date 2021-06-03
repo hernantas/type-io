cf047753ed60d00905191ba4c6493deb3e78c43f
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-06-03 12:43:32 +0700

    Refactor `CodecManager`

---
+++ packages/type-io-core/src/core/CodecManager.ts
@@ -0,0 +1,20 @@
+import { ConstructorValue, TargetType } from '../type'
+import { Codec } from '../type/Codec'
+import { isTargetType } from './type'
+
+export class CodecManager {
+  readonly #codecs: Array<Codec<unknown>> = []
+
+  constructor (codecCtors?: Array<ConstructorValue<Codec<any>>>) {
+    this.push(...(codecCtors ?? []).map(CodecCtor => new CodecCtor()))
+  }
+
+  push (...codecs: Array<Codec<any>>): number {
+    return this.#codecs.push(...codecs)
+  }
+
+  find <T> (target: TargetType<T>): Codec<T, unknown> | undefined {
+    const codecs = this.#codecs.filter(codec => isTargetType(target, codec.target)) as Array<Codec<T, unknown>>
+    return codecs.length > 0 ? codecs[0] : undefined
+  }
+}
