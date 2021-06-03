973e777224feebfc38e5471575766416213c4abb
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-06-03 13:37:37 +0700

    Change constructor argument from `unknown[]` to `any[]`

--- packages/type-io-core/src/type/ConstructorValue.ts
+++ packages/type-io-core/src/type/ConstructorValue.ts
@@ -1,4 +1,4 @@
 /**
  * Type alias for Constructor type with any param
  */
-export type ConstructorValue<T = unknown> = new (...args: unknown[]) => T
+export type ConstructorValue<T = unknown> = new (...args: any[]) => T
