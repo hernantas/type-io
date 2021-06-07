fd7331ac9a46798a926e9cf47afa8e701a80729a
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-06-07 11:34:38 +0700

    Refine docs to `TargetType`

--- packages/type-io-core/src/type/TargetType.ts
+++ packages/type-io-core/src/type/TargetType.ts
@@ -2,6 +2,6 @@ import { ConstructorValue } from './ConstructorValue'
 import { TypeIdentity } from './TypeIdentity'
 
 /**
- * Type of "target" that can be used to determine information about type of `<T>`
+ * Convinient Type of "target" that can be used to determine information about type of `<T>`
  */
 export type TargetType<T = unknown> = ConstructorValue<T> | TypeIdentity<T>
