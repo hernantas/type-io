e0c77b7e01ccce35a833bef0fafecc0df633829d
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-06-10 15:57:39 +0700

    Fix `NodeParser` cannot accept codec

--- packages/type-io-node/src/NodeParser.ts
+++ packages/type-io-node/src/NodeParser.ts
@@ -2,7 +2,7 @@ import { Codec, ConstructorValue, PlainParser } from '@type-io/core'
 import { BufferCodec } from './BufferCodec'
 
 export class NodeParser extends PlainParser {
-  constructor (...codecs: Array<ConstructorValue<Codec<unknown>>>) {
-    super(...codecs, BufferCodec)
+  constructor (...codecCtors: Array<ConstructorValue<Codec<any>>>) {
+    super(...codecCtors, BufferCodec)
   }
 }
