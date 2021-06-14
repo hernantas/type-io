c582c0859af87ea61d6d60c88ca96198e4a30d30
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-06-14 13:39:44 +0700

    Move `target` prop to `Codec`

--- packages/type-io-core/src/type/Codec.ts
+++ packages/type-io-core/src/type/Codec.ts
@@ -1,5 +1,10 @@
 import { Decoder } from './Decoder'
 import { Encoder } from './Encoder'
+import { TargetType } from './TargetType'
 
 export interface Codec<T, O = T, I = unknown> extends Decoder<T, I>, Encoder<T, O> {
+  /**
+   * `TargetType` of this codec
+   */
+  readonly target: TargetType<T>
 }
--- packages/type-io-core/src/type/Decoder.ts
+++ packages/type-io-core/src/type/Decoder.ts
@@ -1,12 +1,6 @@
-import { TargetType } from './TargetType'
 import { CodecOption } from './CodecOption'
 
 export interface Decoder<T, I = unknown> {
-  /**
-   * `TargetType` of this decoder
-   */
-  readonly target: TargetType<T>
-
   /**
    * Decode given value into `<T>` type
    *
