88cf95c83ce9ac103a0221f3e44dc5d8900726ca
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-06-15 11:51:55 +0700

    Refactor `array` testing

--- packages/type-io-core/test/function/array.spec.ts
+++ packages/type-io-core/test/function/array.spec.ts
@@ -1,117 +1,75 @@
 import { expect } from 'chai'
-import { array, PlainParser, Prop } from '../../src'
+import { array, PlainParser } from '../../src'
 
-describe('Parse array', () => {
-  class WithArray {
-    @Prop({ type: array(String) })
-    typeStrings: string[]
-
-    constructor () {
-      this.typeStrings = []
-    }
-  }
-
-  class Basic {
-    @Prop()
-    typeString: string
-
-    constructor (typeString?: string) {
-      this.typeString = typeString ?? ''
-    }
-  }
-
+describe('Array functionality', () => {
   const parser = new PlainParser()
 
-  it('Decode/Encode from plain with array', () => {
-    const plain = {
-      typeStrings: [
-        'This is first string',
-        'This is second string',
-        'This is third string'
-      ]
-    }
-    const decode = parser.decode(plain, WithArray)
-    const encode = parser.encode(decode)
-    expect(decode).to.have.property('typeStrings').and.to.have.members(plain.typeStrings)
-    expect(encode).to.have.property('typeStrings').and.to.have.members(plain.typeStrings)
+  it('Decode/Encode from/to empty array', () => {
+    const input: string[] = []
+    const target = array(String)
+
+    const decoded = parser.decode(input, target)
+    const encoded = parser.encode(decoded, target)
+
+    expect(decoded).to.be.eql(input)
+    expect(encoded).to.be.eql(input)
   })
 
-  it('Decode/Encode from plain array', () => {
-    const plains = [
-      { typeString: 'This is first string' },
-      { typeString: 'This is second string' },
-      { typeString: 'This is third string' }
+  it('Decode/Encode from/to array of string', () => {
+    const input = [
+      'This is first string',
+      'This is second string',
+      'This is third string'
     ]
-    const decode = parser.decode(plains, array(Basic))
-    const encode = parser.encode(decode)
-    expect(decode).to.deep.members(plains)
-    expect(encode).to.deep.members(plains)
+    const target = array(String)
+
+    const decoded = parser.decode(input, target)
+    const encoded = parser.encode(decoded, target)
+
+    expect(decoded).to.have.members(input)
+    expect(encoded).to.have.members(input)
   })
 
-  it('Decode/Encode from empty array', () => {
-    const decode = parser.decode([], array(Basic))
-    const encode = parser.encode(decode, array(Basic))
-    expect(decode).to.be.eql([])
-    expect(encode).to.be.eql([])
-    expect(() => parser.encode([])).to.throws()
-  })
-
-  it('Define class with array property without specify type', () => expect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    class WithArrayNoSpecify {
-      @Prop()
-      typeStrings: string[]
-
-      constructor () {
-        this.typeStrings = []
-      }
-    }
-  }).to.throws())
-
-  class WithArrayOfArray {
-    @Prop({ type: array(array(String)) })
-    typeStrings: string[][]
-
-    constructor () {
-      this.typeStrings = []
-    }
-  }
-
-  it('Decode/Encode from object with array of array of string', () => {
-    const plain = {
-      typeStrings: [
-        [
-          '[Group 1] This is first string',
-          '[Group 1] This is second string',
-          '[Group 1] This is third string'
-        ],
-        [
-          '[Group 2] This is first string',
-          '[Group 2] This is second string',
-          '[Group 2] This is third string'
-        ]
+  it('Decode/Encode from/to array of array of string', () => {
+    const input = [
+      [
+        'This is 1-1 string',
+        'This is 1-2 string',
+        'This is 1-2 string'
+      ],
+      [
+        'This is 1-1 string',
+        'This is 1-2 string',
+        'This is 1-2 string'
       ]
-    }
-    const decode = parser.decode(plain, WithArrayOfArray)
-    const encode = parser.encode(decode)
-    expect(decode).to.have.property('typeStrings').and.to.have.deep.members(plain.typeStrings)
-    expect(encode).to.have.property('typeStrings').and.to.have.deep.members(plain.typeStrings)
+    ]
+    const target = array(array(String))
+
+    const decoded = parser.decode(input, target)
+    const encoded = parser.encode(decoded, target)
+
+    expect(decoded).to.have.deep.members(input)
+    expect(encoded).to.have.deep.members(input)
   })
 
-  it('Decode/Encode from deep array', () => {
-    const plain: string[][][] = []
-    for (let section = 0; section < 5; section++) {
-      plain.push([])
-      for (let group = 0; group < 5; group++) {
-        plain[section].push([])
-        for (let i = 0; i < 5; i++) {
-          plain[section][group].push(`[Section #${section}][Group #${group}] The #${i} string`)
+  it('Decode/Encode from/to deep array of string', () => {
+    const target = array(array(array(String)))
+
+    const input: string[][][] = []
+    for (let x = 0; x < 5; x++) {
+      input.push([])
+      for (let y = 0; y < 5; y++) {
+        input[x].push([])
+        for (let z = 0; z < 5; z++) {
+          input[x][y].push(`Section x:${x} y:${y} z:${z}`)
         }
       }
     }
-    const decode = parser.decode(plain, array(array(array(String))))
-    const encode = parser.encode(decode)
-    expect(decode).to.have.deep.members(plain)
-    expect(encode).to.have.deep.members(plain)
+
+    const decoded = parser.decode(input, target)
+    const encoded = parser.encode(decoded, target)
+
+    expect(decoded).to.have.deep.members(input)
+    expect(encoded).to.have.deep.members(input)
   })
 })
