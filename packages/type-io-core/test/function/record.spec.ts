f46a05430b2b5518b664e4a2928c8dd4546847b0
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2021-06-15 11:17:16 +0700

    Refactor `Record` testing

---
+++ packages/type-io-core/test/function/record.spec.ts
@@ -0,0 +1,84 @@
+import { expect } from 'chai'
+import { PlainParser, record } from '../../src'
+
+describe('Record functionality', () => {
+  const parser = new PlainParser()
+
+  it('Decode/Encode from/to plain object', () => {
+    const plain = {
+      _string: 'This is string',
+      _number: 8080,
+      _boolean: true,
+      _date: new Date()
+    }
+    const target = record({
+      _string: String,
+      _number: Number,
+      _boolean: Boolean,
+      _date: Date
+    })
+
+    const decoded = parser.decode(plain, target)
+    const encoded = parser.encode(decoded, target)
+
+    expect(decoded).to.have.property('_string', plain._string)
+    expect(decoded).to.have.property('_number', plain._number)
+    expect(decoded).to.have.property('_boolean', plain._boolean)
+    expect(decoded).to.have.property('_date', plain._date)
+
+    expect(encoded).to.have.property('_string', plain._string)
+    expect(encoded).to.have.property('_number', plain._number)
+    expect(encoded).to.have.property('_boolean', plain._boolean)
+    expect(encoded).to.have.property('_date', plain._date)
+  })
+
+  it('Decode/Encode from/to plain object', () => {
+    const plain = {
+      _string: 'This is string',
+      _number: 8080,
+      _boolean: true,
+      _date: new Date(),
+      _nested: {
+        _string: 'This is string',
+        _number: 8080,
+        _boolean: true,
+        _date: new Date()
+      }
+    }
+    const target = record({
+      _string: String,
+      _number: Number,
+      _boolean: Boolean,
+      _date: Date,
+      _nested: record({
+        _string: String,
+        _number: Number,
+        _boolean: Boolean,
+        _date: Date
+      })
+    })
+
+    const decoded = parser.decode(plain, target)
+    const encoded = parser.encode(decoded, target)
+
+    expect(decoded).to.have.property('_string', plain._string)
+    expect(decoded).to.have.property('_number', plain._number)
+    expect(decoded).to.have.property('_boolean', plain._boolean)
+    expect(decoded).to.have.property('_date', plain._date)
+    expect(decoded).to.have.property('_nested')
+    expect(decoded).to.have.nested.property('_nested._string', plain._nested._string)
+    expect(decoded).to.have.nested.property('_nested._number', plain._nested._number)
+    expect(decoded).to.have.nested.property('_nested._boolean', plain._nested._boolean)
+    expect(decoded).to.have.nested.property('_nested._date', plain._nested._date)
+
+    expect(encoded).to.have.property('_string', plain._string)
+    expect(encoded).to.have.property('_number', plain._number)
+    expect(encoded).to.have.property('_boolean', plain._boolean)
+    expect(encoded).to.have.property('_date', plain._date)
+    expect(encoded).to.have.property('_nested')
+    expect(encoded).to.have.nested.property('_nested._string', plain._nested._string)
+    expect(encoded).to.have.nested.property('_nested._number', plain._nested._number)
+    expect(encoded).to.have.nested.property('_nested._boolean', plain._nested._boolean)
+    expect(encoded).to.have.nested.property('_nested._date', plain._nested._date)
+  })
+})
